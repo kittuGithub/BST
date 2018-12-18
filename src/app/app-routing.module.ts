e1fc870cfc4c0ae60c6a94059d525f2f0fa8e71d
Budamkayala, Krishna (ES) <krishna.budamkayala@adp.com>
2018-12-18 11:09:02 +0530

    login routing issue resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     }]
   }, {
     path: 'login',
-    component: LoginPageComponent
+    component: LoginPageComponent,
   }
 ];
 
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -16,9 +16,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       const isLoggedIn = this.authService.isLoggedIn;
-      if (isLoggedIn) {
-        this.router.navigate(['']);
-      } else {
+      if (!isLoggedIn) {
         this.router.navigate(['login']);
       }
     return isLoggedIn;
